674733ebea070d75baa09a0fa0525eaf3db149d2
Alina Shilo <alinashilo98@gmail.com>
2022-02-23 21:47:35 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create name of dream team based on the names of its members
  *  
@@ -13,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  return members.reduce((acc, cur) => {
+    if (typeof cur === 'string') {
+      acc.push(cur.trim()[0].toUpperCase());
+    }
+    return acc;
+  }, []).sort().join('');
 }
 
 module.exports = {
